6da2884bd79bf675f7d24c4224502325819fc262
Ngoc Duc Huynh <ndhuynh@kitasan.laas.fr>
2014-04-11 16:13:09 +0200

    add files

---
+++ src/Mathematics/Bsplines.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+#include <jrl/mal/matrixabstractlayer.hh>
+
+#include <Mathematics/StepOverPolynome.hh>
+
+#include <Debug.hh>
+
+using namespace::std;
+using namespace::PatternGeneratorJRL;
---
+++ src/Mathematics/Bsplines.hh
@@ -0,0 +1,13 @@
+/** \file StepOverPolynome.h
+    \brief Polynomes object for generating foot and hip trajectories while stepping over. */
+
+
+#ifndef _STEPOVER_POLYNOME_H_
+#define _STEPOVER_POLYNOME_H_
+
+#include <vector>
+
+#include <jrl/mal/matrixabstractlayer.hh>
+
+
+#include <Mathematics/Polynome.hh>
