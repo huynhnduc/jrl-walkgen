f81996d08382491907d0a81e3ab81f42989c2410
Huynh Ngoc Duc <huynhnduc100@yahoo.com.vn>
2014-05-01 14:18:53 +0200

    Update Bsplines.cpp

--- src/Mathematics/Bsplines.cpp
+++ src/Mathematics/Bsplines.cpp
@@ -9,7 +9,6 @@ using namespace::std;
 using namespace::PatternGeneratorJRL;
 
 
-
 Bsplines::Bsplines(int degree, std::vector<Point> control_points)
 {
     m_degree = degree;
@@ -22,6 +21,15 @@ Bsplines::Bsplines(int degree, std::vector<Point> control_points)
 
 Bsplines::~Bsplines()
 {
+}
+
+void Bsplines::GenerateDegree()
+{
+    m_degree = (m_knot_vector.size()-1) - (m_control_points.size()-1) -1;
+    if (m_degree < 0)
+    {
+        cout << " Attention !! degree is smaller than 0 " << endl;
+    }
 }
 
 void Bsplines::GenerateKnotVector(std::string method)
@@ -47,21 +55,19 @@ void Bsplines::GenerateKnotVector(std::string method)
             {
                 if (i == 0)
                 {
-                    set_of_pam[0] = 0.0;
+                    set_of_pam.push_back(0.0);
                 }
                 else if (i == m_control_points.size()-1)
                 {
-                    set_of_pam[i] = 1;
+                    set_of_pam.push_back(1.0);
                 }
                 else
                 {
                     D += sqrt ( sqrt(((m_control_points[i].x - m_control_points[i+1].x)*(m_control_points[i].x - m_control_points[i+1].x)) +
                               ((m_control_points[i].y - m_control_points[i+1].y)*(m_control_points[i].y - m_control_points[i+1].y)) ) );
-                    set_of_pam[i] = D/L;
+                    set_of_pam.push_back(D/L);
                 }
             }
-            /*generate a knot vector*/
-            //cout << m_control_points.size() << endl;
 
             m_knot_vector.clear();
             double U = 0.0;
@@ -81,10 +87,9 @@ void Bsplines::GenerateKnotVector(std::string method)
                         U +=set_of_pam[i];
                         i++;
                     }
-                    m_knot_vector.push_back((U/m_degree));
+                    m_knot_vector.push_back(U/double(m_degree));
                 }
             }
-            //cout << m_control_points.size() << endl;
 
             for (i=0;i<=m_degree;i++)
             {
@@ -95,13 +100,14 @@ void Bsplines::GenerateKnotVector(std::string method)
                 cout << "Knot vector cant be created. m_control_points.size()-1>=m_degree "<< endl;
                 m_knot_vector.clear();
             }
+
         }
 
-        else if (method  == "universal")
+        else if (method =="universal")
         {
             m_knot_vector.clear();
             cout << "universal" << endl;
-            double U;
+            double U=0;
             for (i=0;i<=m_degree;i++)
             {
                 m_knot_vector.push_back(0.0);
@@ -115,62 +121,69 @@ void Bsplines::GenerateKnotVector(std::string method)
             {
                 m_knot_vector.push_back(1.0);
             }
-        }
 
+        }
 }
 
 double *Bsplines::ComputeBasisFunction(double t)
 {
     double **m_basis_function;
     m_basis_function = new double* [m_degree+1];
-    int i,j;
+    int i,j,n;
 
     if (m_degree!= m_knot_vector.size() - m_control_points.size() -1 )
     {
         cout << "The parameters are not compatibles. Please recheck " << endl;
-        return C;
+        return NULL;
     }
-    
-    
         for(j=0;j <= m_degree;j++)
         {
-            int n = m_knot_vector.size() - 1 -j -1;
+            n = m_knot_vector.size() - 1 -j -1;
             m_basis_function[j] = new double[n+1];
-            cout << "order  " ; cout << j << endl;
-            cout << "n control point  " ; cout << n << endl;
+            //cout << "order  " ; cout << j << endl;
+            //cout << "n control point  " ; cout << n << endl;
 
             for(i=0;i <= n;i++)
             {
-                if (j == 0 && m_knot_vector[i]<= t && t< m_knot_vector[i+1])
+               if (j == 0 && m_knot_vector[i]<= t && t< m_knot_vector[i+1])
                 {
                     m_basis_function[j][i] = 1.0;
+                    //cout << i << " "<< j << " "<<m_basis_function[j][i] << endl;
                 }
+
                 else if (j == 0)
                 {
                     m_basis_function[j][i] = 0.0;
+                    //cout << i << " "<< j << " "<<m_basis_function[j][i] << endl;
                 }
-                else if (j!=0)
+                else if (j != 0)
                 {
                     if ( m_knot_vector[i] != m_knot_vector[i+j] && m_knot_vector[i+j+1] != m_knot_vector[i+1] )
-                        m_basis_function[j][i]= m_basis_function[j-1][i]*(t - m_knot_vector[i])/(m_knot_vector[i+j]-m_knot_vector[i])
-                                                + m_basis_function[j-1][i+1]*(m_knot_vector[i+j+1] - t)/ (m_knot_vector[i+j+1]-m_knot_vector[i+1] );
+                        m_basis_function[j][i]= m_basis_function[j-1][i]*((t - m_knot_vector[i])/(m_knot_vector[i+j]-m_knot_vector[i]))
+                                                + m_basis_function[j-1][i+1]*((m_knot_vector[i+j+1] - t)/ (m_knot_vector[i+j+1]-m_knot_vector[i+1] ));
 
                     else if ( m_knot_vector[i] == m_knot_vector[i+j] && m_knot_vector[i+j+1] == m_knot_vector[i+1] )
                         m_basis_function[j][i] = 0.0;
 
                     else if (m_knot_vector[i] == m_knot_vector[i+j])
-                        m_basis_function[j][i]= m_basis_function[j-1][i+1]*(m_knot_vector[i+j+1] - t)/ (m_knot_vector[i+j+1]-m_knot_vector[i+1] );
+                        m_basis_function[j][i]= m_basis_function[j-1][i+1]*((m_knot_vector[i+j+1] - t)/ (m_knot_vector[i+j+1]-m_knot_vector[i+1] ));
 
                     else if (m_knot_vector[i+j+1] == m_knot_vector[i+1])
-                        m_basis_function[j][i]= m_basis_function[j-1][i]*(t - m_knot_vector[i])/(m_knot_vector[i+j]-m_knot_vector[i]);
+                        m_basis_function[j][i]= m_basis_function[j-1][i]*((t - m_knot_vector[i])/(m_knot_vector[i+j]-m_knot_vector[i]));
+
+                    //cout << i << " "<< j << " "<<m_basis_function[j][i] << endl;
                 }
             }
+        }
+        for(j=0;j < m_degree;j++)
+        {
+            delete[] m_basis_function[j];
         }
 
 return m_basis_function[m_degree];
 }
 
-Point Bsplines::ComputeBplines(double t)
+Point Bsplines::ComputeBsplines(double t)
 {
     double *m_basis_function = ComputeBasisFunction(t);
 
@@ -180,7 +193,6 @@ Point Bsplines::ComputeBplines(double t)
         cout << "The parameters are not compatibles. Please recheck " << endl;
         return C;
     }
-    
     if (m_degree == ((m_knot_vector.size()-1) - (m_control_points.size()-1)- 1) )
         {
             for (int i=0;i<m_control_points.size();i++)
@@ -203,14 +215,10 @@ Bsplines Bsplines::DerivativeBsplines()
     {
         for (int i=0;i<m_control_points.size()-1;i++)
         {
-
-            //cout << i<< endl;
             T.x = ((m_control_points[i+1].x - m_control_points[i].x)*double(m_degree) )/ (m_knot_vector[i+m_degree+1] - m_knot_vector[i+1]);
-            //cout << i<< endl;
+
             T.y = ((m_control_points[i+1].y - m_control_points[i].y)*double(m_degree) )/ (m_knot_vector[i+m_degree+1] - m_knot_vector[i+1]);
-            //cout << i<< endl;
             Q.push_back(T);
-            //cout << i << endl;
         }
         Bsplines B(m_degree-1,Q);
         std::vector<double> new_knot_vector(m_knot_vector.begin()+1,m_knot_vector.end()-1);
@@ -229,12 +237,12 @@ Bsplines Bsplines::DerivativeBsplines()
 
 }
 
-void Bsplines::SetDegree(int &degree)
+void Bsplines::SetDegree(int degree)
 {
     m_degree = degree;
 }
 
-void Bsplines::SetControlPoints(std::vector<Point> &control_points)
+void Bsplines::SetControlPoints(std::vector<Point> control_points)
 {
     if (control_points.size()>=2)
     {
@@ -246,7 +254,7 @@ void Bsplines::SetControlPoints(std::vector<Point> &control_points)
     }
 }
 
-void Bsplines::SetKnotVector(std::vector<double> &knot_vector)
+void Bsplines::SetKnotVector(std::vector<double> knot_vector)
 {
     m_knot_vector = knot_vector;
 }
